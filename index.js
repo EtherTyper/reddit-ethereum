e0b4b7d4eb3b3b8c1b64bd10c5c01a9878c43196
Eli Bradley <elibradley@utexas.edu>
2022-02-17 16:48:44 -0600

    Stuff

--- index.js
+++ index.js
@@ -16,16 +16,23 @@ const argv = yargs(hideBin(process.argv))
     type: "string",
   }).argv;
 
+const api_endpoint = (() => {
+  if (argv.network == "mainnet") {
+    return "api.etherscan.io";
+  } else {
+    return `api-${argv.network}.etherscan.io`;
+  }
+})();
+
 const api_response = await (
-    await fetch(
-      `https://api-${argv.network}.etherscan.io/api?module=contract&action=getsourcecode&address=${argv.contract}&apikey=${argv.apikey}`
-    )
-  ).text();
+  await fetch(
+    `https://${api_endpoint}/api?module=contract&action=getsourcecode&address=${argv.contract}&apikey=${argv.apikey}`
+  )
+).text();
 await util.promisify(fs.writeFile)("./api.json", api_response);
 
 const api_result = JSON.parse(api_response).result[0];
 const compiler_version = api_result.CompilerVersion;
-console.log(compiler_version);
 const source_field = api_result.SourceCode.slice(1, -1);
 await util.promisify(fs.writeFile)("./compiler_version.txt", compiler_version);
 await util.promisify(fs.writeFile)("./source.json", source_field);
@@ -33,7 +40,6 @@ await util.promisify(fs.writeFile)("./source.json", source_field);
 const sources = JSON.parse(source_field).sources;
 
 for (const contract in sources) {
-    await util.promisify(fs.mkdir)(path.dirname(contract), { recursive: true });
-    await util.promisify(fs.writeFile)(contract, sources[contract].content);
-    console.log(`Writing ${contract}.`);
+  await util.promisify(fs.mkdir)(path.dirname(contract), { recursive: true });
+  await util.promisify(fs.writeFile)(contract, sources[contract].content);
 }
